2c6753b6864de71e9dff7847db64fcb92a854def
Francesco Di Sales <f.disales@hotmail.com>
2020-10-20 00:36:38 +0200

    Update main.c

--- profbomb/main.c
+++ profbomb/main.c
@@ -4,23 +4,21 @@
 
 #define fourGB 4000000000
 
-//warning: this piece of code will damage your ssd with a lot of runs
+//warning: this piece of code will damage your ssd when you run it multiple times
 
 int main()
 {
  
-
  long int i=0;
  printf("wait.... \n");
- FILE* bombFile=fopen("miofile.txt","w");
+ FILE* bombFile=fopen("myFile.txt","w");
 
 
-  for(i=0;i<4000000000;i++)
+  for(i=0;i<fourGB;i++)
    fprintf(bombFile,"a");
  
  
  fclose(bombFile);
-
-
  return 0;
+ 
 }
