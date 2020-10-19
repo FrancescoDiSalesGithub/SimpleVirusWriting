4dd59a865fede61b9c21b0513bca6d9edfabf198
FrancescoDiSalesDEV <f.disales@hotmail.com>
2020-10-20 01:10:52 +0200

    added Hidden Message virus

---
+++ hideMessagePicture/checkHiddenMessage.sh
@@ -0,0 +1,1 @@
+hexdump -c image.jpeg
---
+++ hideMessagePicture/main.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    FILE* imageFile = fopen("image.jpeg","ab");
+    
+    char* myMessage = "you have been hacked";
+
+    if(imageFile == NULL)
+    {
+        printf("file not found \n");
+        return -1;
+    }
+        
+    fwrite(myMessage,sizeof(myMessage),strlen(myMessage),imageFile);
+    fclose(imageFile);
+
+    return 0;
+
+}
