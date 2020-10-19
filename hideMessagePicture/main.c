7869ed1da679495721a5c2d365a08be283edde3d
Francesco Di Sales <f.disales@hotmail.com>
2020-10-20 01:51:15 +0200

    Update main.c

--- hideMessagePicture/main.c
+++ hideMessagePicture/main.c
@@ -4,13 +4,11 @@
 int main()
 {
     FILE* imageFile = fopen("image.jpeg","ab");
-    
     char* myMessage = "you have been hacked";
 
     if(imageFile == NULL)
     {
-        printf("file not found \n");
-        return -1;
+       return -1;
     }
         
     fwrite(myMessage,sizeof(*myMessage),strlen(myMessage),imageFile);
